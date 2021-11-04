40de197c7b9e2fe1b493739611d6cc351a1da7f6
Harlen Hobbs <hhobbs@affirma.com>
2021-11-04 07:55:13 -0400

    modifying Roll.js

--- src/pages/Roll.js
+++ src/pages/Roll.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 
 
 const Roll = () => {
+  const [mod20, setMod20] =useState(0);
   const [roll, setRoll] = useState(0);
   const [data, setData] = useState(0);
 
@@ -21,8 +22,11 @@ const Roll = () => {
   return (
     <>
     <form class="col-lg-6 offset-lg-3 " onSubmit={handleSubmit}>
-    <input type="submit" />
-    <button onClick={() => handleSubmit(1, 200)} type="button">Roll D20</button>
+    <label>Modifier :
+      <input type="number" value={mod20} onChange={(e) => setMod20(parseInt(e.target.value))} />
+    </label> <br />
+    <button onClick={() => handleSubmit(1, 3)} type="button">Roll D20</button>
+    <h1 class="d20">{roll}{ mod20 >= 0 ? "+" : " " }{mod20} = {roll + mod20}</h1>
   </form>
     <button type="button">Roll D20</button>
       {data &&
