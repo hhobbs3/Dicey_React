053ad2aa8e4e667aa5c50fa28e0d0975362e55cf
Harlen Hobbs <hhobbs@affirma.com>
2021-11-23 08:20:18 -0500

    adding readme

---
+++ readme.txt
@@ -0,0 +1,8 @@
+Connects to AWS components and graphQL via mockup
+
+To mock up graphQL, run: npx json-server --watch data/db.json --port 8000
+    Note that the graphQL functionality has been temporarily disabled in graphQLExample.json
+
+To run the program: npm run start
+
+Recently added Tailwind functionality, causing much of the original css to not work as it once did.
--- src/pages/graphQLExample.js
+++ src/pages/graphQLExample.js
@@ -1,4 +1,48 @@
+/*
+import { useState } from "react";
+import { useHistory } from 'react-router-dom';
+
+
+
+/*
+{
+  books{
+    name
+    author{
+      name
+      age
+    }
+  },
+  authors{
+    name
+  }
+}
+*/
+/*
 const GraphQLExample = () => {
+    const testQuery = "{books{ name author{ name age } }, authors{ name } }";
+    const handleSubmit = (e) => {
+        e.preventDefault(); // prevents the page from refreshing
+        const [isPending, setIsPending] = useState(false);
+        const history = useHistory();
+
+        setIsPending(true);
+
+        console.log(blog);
+        fetch('http://localhost:4000', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: testQuery
+        }).then(() => {
+            console.log('new blog added');
+            setIsPending(false);
+            // history.go(-1); // go back one page
+            history.push('/');
+        })
+
+        
+    }
+
     return ( 
         <div className="graphql-example">
             <p>GraphQL Example</p>
@@ -7,3 +51,4 @@ const GraphQLExample = () => {
 }
  
 export default GraphQLExample;
+*/
